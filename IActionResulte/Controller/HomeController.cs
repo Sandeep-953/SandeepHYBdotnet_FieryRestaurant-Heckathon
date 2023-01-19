339a63d0354954f05be1ff9e08a3fc3405202921
Sandeep-953 <109785841+Sandeep-953@users.noreply.github.com>
2023-01-19 12:43:35 +0530

    Add project files.

---
+++ IActionResulte/Controller/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResulte.Controller
+{
+    public class HomeController : Controller
+    {
+        [Route("book")]
+        public IActionResult Index()
+        {
+            // Book id shuld be applid
+            if(!Request.Query.ContainsKey("bookid"))
+        }
+    }
+}
